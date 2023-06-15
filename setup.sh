acb7a19bae077652149c1476577aa18aa37bcac4
Kaushik R Bangera <50287102+NeuronEnix@users.noreply.github.com>
2023-06-15 08:27:01 +0530

    feat: added default dir

--- setup.sh
+++ setup.sh
@@ -1,2 +1,10 @@
 bashrc_custom='if [ -f ~/adoc/dev-env/bashrc_custom ]; then . ~/adoc/dev-env/bashrc_custom; fi'
-if ! grep -qxF "$bashrc_custom" ~/.bashrc; then echo "\n$bashrc_custom" >> ~/.bashrc; fi
+if ! grep -qxF "$bashrc_custom" ~/.bashrc; then printf "\n# dev-env bashrc_custom\n$bashrc_custom\n" >> ~/.bashrc; fi
+
+# defualt directory structure
+mkdir -p ~/adoc/proj
+mkdir -p ~/adoc/cloud/aws
+mkdir -p ~/adoc/cloud/gcp
+mkdir -p ~/adoc/cloud/az
+mkdir -p ~/adoc/mounts
+mkdir -p ~/adoc/temp
