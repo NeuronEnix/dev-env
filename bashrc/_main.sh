bab863fdc30e659f9fe944f06ae2a5100f0eb8c1
Kaushik R Bangera <>
2023-12-28 08:40:04 +0530

    feat(bashrc): enable extra.sh by default

--- bashrc/_main.sh
+++ bashrc/_main.sh
@@ -1,6 +1,7 @@
 # Uncomment to enable command
+
+. ~/dev-env/bashrc/extra.sh
 . ~/dev-env/bashrc/_service.sh
 # . ~/dev-env/bashrc/docker.sh
-# . ~/dev-env/bashrc/extra.sh
 # . ~/dev-env/bashrc/kubectl.sh
 # . ~/dev-env/bashrc/minikube.sh
---
+++ bashrc/extra.sh
@@ -0,0 +1,2 @@
+alias mnt-ec2='sshfs ubuntu@vm.kaushikbangera.com:/home/ubuntu/adoc ~/adoc-ec2'
+alias mnt-ec2-remove='fusermount3 -u ~/adoc-ec2'
