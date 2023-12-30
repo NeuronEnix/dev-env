534d4342ea73f8cffe7699f7ef56ee52aa8bbd37
Kaushik R Bangera <kaushikrb@duck.com>
2023-12-30 19:51:22 +0530

    feat: backup script -> dev-env, kms, adoc

---
+++ backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+backup_dir="$HOME/my-backup/$(date +"%Y_%m_%d-%H_%M_%S")"
+mkdir -p "$backup_dir"
+
+# Backup ~/dev-env
+cd ~/dev-env
+sudo zip -r "$backup_dir/dev-env.zip" .
+
+# Backup ~/adoc
+cd ~/adoc/kms
+sudo zip -r "$backup_dir/kms.zip" .
+
+# Backup ~/adoc
+cd ~/adoc
+node minify.js
+sudo zip -r "$backup_dir/adoc.zip" .
