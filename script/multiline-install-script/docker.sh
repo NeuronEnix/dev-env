bb7c54d7aa45330ec028f1d28da156128df853c2
Kaushik R Bangera <kaushikrb@duck.com>
2023-12-31 11:12:09 +0530

    feat(script): install > docker > use convenient script

--- script/multiline-install-script/docker.sh
+++ script/multiline-install-script/docker.sh
@@ -1,38 +1,43 @@
-# https://docs.docker.com/engine/install/ubuntu/#install-from-a-package
+# https://docs.docker.com/engine/install/ubuntu/
 
+# ### Fast Install --dry-run
+#   curl -fsSL https://get.docker.com -o get-docker.sh
+#   sudo sh ./get-docker.sh --dry-run
+
+
+### Fast Install (only for testing / development)
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sudo sh get-docker.sh
 
 # ##################################
-# # Fast install
-# curl -fsSL https://get.docker.com -o get-docker.sh
-# sudo sh get-docker.sh
+# ### Manual install
+# # Step 1: Add Docker's official GPG key:
+#   sudo apt-get update
+#   sudo apt-get install ca-certificates curl gnupg
+#   sudo install -m 0755 -d /etc/apt/keyrings
+#   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+#   sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-#################################
-# Manual Install
-# Add Docker's official GPG key:
-sudo apt-get install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+# # Step 2: Add the repository to Apt sources:
+# # Note: If you use an Ubuntu derivative distro, such as Linux Mint,
+# # you may need to use UBUNTU_CODENAME instead of VERSION_CODENAME.
+#   echo \
+#     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+#     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+#     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#   sudo apt-get update
 
-# Add the repository to Apt sources:
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-# Note: If you use an Ubuntu derivative distro, such as Linux Mint, you may need to use UBUNTU_CODENAME instead of VERSION_CODENAME.
+# # Step 3: Install the Docker packages
+#   sudo apt-get -y install docker-ce docker-ce-cli containerd.io \
+#     docker-buildx-plugin docker-compose-plugin
 
-# Install Docker Engine
-# Update the apt package index:
-sudo apt-get update
+# # # Step 4: (Opetional) Verify that the Docker Engine installation
+# # sudo docker run hello-world
 
-# Install Docker Engine, containerd, and Docker Compose. ( Latest )
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 ##################################
-# Verify that the Docker Engine installation is successful by running the hello-world image.
-# sudo docker run hello-world
-
-##################################
-# Don't wanna use sudo ?
+# ### Don't wanna use sudo ?
 # sudo usermod -aG docker $USER
+# # Or haven't tested: dockerd-rootless.sh --experimental
+# # Or this, came accross while installing: dockerd-rootless-setuptool.sh install
+rm -f get-docker.sh
