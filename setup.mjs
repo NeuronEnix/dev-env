81b42ccf24489dfa6763a3205b354c3a47a022c0
Kaushik R Bangera <kaushikrb@duck.com>
2024-01-22 10:22:31 +0000

    integrated zx to install package

--- setup.mjs
+++ setup.mjs
@@ -15,9 +15,67 @@ while (true) {
       await setupSymlink()
       await setupService()
     } break
-    case "Install": echo("install"); break
+    case "Install": await installPackage(); break
     default: echo("Incorrect option")
   }
+}
+
+async function installPackage() {
+  echo("\n Instal Package")
+  const path = "script/install-script"
+  const pkgList = [
+    "Curl:curl:--version:apt:curl",
+    "Wget:wget:--version:apt:wget",
+    "Git:git:--version:apt:git",
+
+    "VS Code:code:-v:snap:code --classic",
+    "Sublime:subl:-v:snap:sublime-text --classic",
+    "Docker:docker:-v:file:docker.sh",
+    "Go( gvm ):gvm:version:file:go-gvm.sh",
+    "Kubectl:kubectl:version --client:file:kubectl.sh",
+    "Minikube:minikube:version:file:minikube.sh",
+    "AWS cli:aws:--version:file:aws-cli.sh",
+
+    "Obsidian:command:-v obsidian:snap:obsidian --classic",
+    "Google Chrome:google-chrome:--version:file:google-chrome.sh",
+    "Postman:command:-v postman:snap:postman",
+    "MongoDB Compass:mongodb-compass:--version:file:mongodb-compass.sh",
+    "Another Redis Desktop Manager:command:-v another-redis-desktop-manager:snap:another-redis-desktop-manager",
+    "Signal Desktop:command:-v signal-desktop:file:signal.sh",
+    "Slack:slack:-v:snap:slack",
+  ]
+  const installedPkg = []
+  const availablePkg = []
+  for (const pkg of pkgList) {
+    const [label, name, arg, type, installCommand] = pkg.split(":")
+    if (await isInstalled(name, arg)) installedPkg.push(pkg)
+    else availablePkg.push(pkg)
+  }
+
+  echo("\n\nInstalled Pkg")
+  installedPkg.forEach((pkg, i) => echo(`${i + 1}. ${pkg.split(":")[0]}`))
+
+  echo("\nAvailable Pkg")
+  availablePkg.forEach((pkg, i) => echo(`${i + 1}. ${pkg.split(":")[0]}`))
+
+  const choice = await question('Choose (space separated): ')
+  $.verbose = true
+  await $`sudo apt update`
+  for (const pkg of choice.split(" ")) {
+    const [label, name, arg, type, installCommand] = availablePkg[parseInt(pkg) - 1].split(":")
+    echo(`Install ${label}`)
+    switch (type) {
+      case "apt": await $`sudo apt install ${["-y", installCommand]}`; break;
+      case "snap": await $`sudo snap install ${installCommand}`; break;
+      case "file": await $`bash script/multiline-install-script/${installCommand}`; break;
+    }
+  }
+  $.verbose = false
+}
+
+async function isInstalled(cmd, arg) {
+  const result = await $`${cmd} ${arg.split(" ")}`.exitCode
+  return result == 0
 }
 
 async function setupToBashrc() {
@@ -70,7 +128,7 @@ async function setupService() {
   echo(" -> Ok: created .env copies")
 
   if (await $`docker -v`.exitCode != 0)
-    return echo(" -> warn: Install docker and this again")
+    return echo(" -> warn: Install docker and run this again")
 
   await $`sudo docker network create dev`.nothrow()
   echo(" -> Ok: create network")
