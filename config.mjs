f8fd1237bd09e6c3b6c83250bda142913481d17b
Kaushik R Bangera <kaushikrb909@gmail.com>
2024-07-16 00:03:05 +0530

    feat(setup): installer metadata to config; refactor setup.mjs

--- config.mjs
+++ config.mjs
@@ -3,7 +3,8 @@ const backupDir = `${os.homedir()}/abkp/${os.hostname()}__${ts.toISOString().spl
 const symlinkDir = `${os.homedir()}/dev-env/symlink`
 export const config = {
   ts, backupDir, symlinkDir,
-
+  setupOption: ["Setup Environment", "Install Default Pkg", "Install Apps"],
+  defaultPkg: ["curl", "wget", "ffmpeg", "unzip"],
   file: [
     { path: `${os.homedir()}/dev-env/bashrc/_custom.sh` },
   ],
@@ -20,45 +21,93 @@ export const config = {
   ],
 
   symlinkList: [
-    {
-      at: `${os.homedir()}/adoc/minify.cjs`,
-      path: `${os.homedir()}/dev-env/script/minify.cjs`
-    },
-    {
-      at: `${symlinkDir}/bashrc.sh`,
-      path: `${os.homedir()}/.bashrc`
-    },
+    { at: `${os.homedir()}/adoc/minify.cjs`, path: `${os.homedir()}/dev-env/script/minify.cjs` },
+    { at: `${symlinkDir}/bashrc.sh`, path: `${os.homedir()}/.bashrc` },
 
     {
-      at: `${symlinkDir}/app`,
-      backupAs: "sym-app",
-      path: `${os.homedir()}/app`,
-      restore: false
+      at: `${symlinkDir}/app`, path: `${os.homedir()}/app`,
+      backupAs: "sym-app", restore: false
     },
     {
-      at: `${symlinkDir}/ssh`,
+      at: `${symlinkDir}/ssh`, path: `${os.homedir()}/.ssh`,
       backupAs: "sym-ssh",
-      path: `${os.homedir()}/.ssh`
+
     },
     {
-      at: `${symlinkDir}/sublime`,
+      at: `${symlinkDir}/sublime`, path: `${os.homedir()}/.config/sublime-text/Local`,
       backupAs: "sym-sublime",
-      path: `${os.homedir()}/.config/sublime-text/Local`
     },
     {
-      at: `${symlinkDir}/syncthing`,
-      backupAs: "sym-syncthing",
-      restore: false,
+      at: `${symlinkDir}/syncthing`, path: `${os.homedir()}/.local/state/syncthing`,
+      backupAs: "sym-syncthing", restore: false,
       systemctlStop: "syncthing",
-      path: `${os.homedir()}/.local/state/syncthing`,
+
     },
   ],
 
   appList: [
+    // From Snap
     {
-      name: "syncthing",
-      bin: `${os.homedir()}/app/syncthing/syncthing`,
-      systemctlScript: `${os.homedir()}/dev-env/script/systemctl/syncthing.sh`
+      name: "Another Redis Desktop Manager", bin: "/snap/bin/another-redis-desktop-manager",
+      install: { from: "snap", pkg: ["another-redis-desktop-manager"] },
+    },
+    {
+      name: "Obsidian", bin: "/snap/bin/obsidian",
+      install: { from: "snap", pkg: ["obsidian", "--classic"] },
+    },
+    {
+      name: "Postman", bin: "/snap/bin/postman",
+      install: { from: "snap", pkg: ["postman"] },
+    },
+    {
+      name: "Slack", bin: "/snap/bin/slack",
+      install: { from: "snap", pkg: ["slack"] },
+    },
+    {
+      name: "Sublime", bin: "/snap/bin/subl",
+      install: { from: "snap", pkg: ["sublime-text", "--classic"] },
+    },
+    {
+      name: "VS Code", bin: "/snap/bin/code",
+      install: { from: "snap", pkg: ["code", "--classic"] },
+    },
+
+    // From File
+    {
+      name: "AWS Cli", bin: "/usr/local/bin/aws",
+      install: { from: "file", path: `${os.homedir()}/dev-env/script/multiline-install-script/aws-cli.sh` },
+    },
+    {
+      name: "Docker", bin: "/usr/bin/docker",
+      install: { from: "file", path: `${os.homedir()}/dev-env/script/multiline-install-script/docker.sh` },
+    },
+    {
+      name: "Flatpak", bin: "/usr/bin/flatpak",
+      install: { from: "file", path: `${os.homedir()}/script/multiline-install-script/flatpak.sh` },
+    },
+    {
+      name: "Google Chrome", bin: "/usr/bin/google-chrome",
+      install: { from: "file", path: `${os.homedir()}/dev-env/script/multiline-install-script/google-chrome.sh` },
+    },
+    {
+      name: "Kubectl", bin: "/usr/bin/kubectl",
+      install: { from: "file", path: `${os.homedir()}/dev-env/script/multiline-install-script/kubectl.sh` },
+    },
+    {
+      name: "Minikube", bin: "/usr/bin/minikube",
+      install: { from: "file", path: `${os.homedir()}/dev-env/script/multiline-install-script/minikube.sh` },
+    },
+    {
+      name: "Mongodb Compass", bin: "/usr/bin/mongodb-compass",
+      install: { from: "file", path: `${os.homedir()}/dev-env/script/multiline-install-script/mongodb-compass.sh` },
+    },
+    {
+      name: "Syncthing", bin: `${os.homedir()}/app/syncthing/syncthing`,
+      install: { from: "file", path: `${os.homedir()}/dev-env/script/multiline-install-script/syncthing.sh` },
+      systemctl: {
+        name: "syncthing",
+        script: `${os.homedir()}/dev-env/script/systemctl/syncthing.service`
+      }
     },
   ]
 }
--- setup.mjs
+++ setup.mjs
@@ -3,86 +3,29 @@
 import 'zx/globals'
 import { config } from './config.mjs'
 $.verbose = false
+$`sudo apt update`;
 
 while (true) {
   echo("\nDevelopment Environment")
-  const option = "Setup Environment, Install Default Pkg, Install Pkg".split(", ")
-  option.forEach((opt, i) => echo(`${i + 1}. ${opt}`))
+  config.setupOption.forEach((opt, i) => echo(`${i + 1}. ${opt}`))
   const choice = await question('Choose: ')
-  switch (option[parseInt(choice) - 1]) {
+
+  switch (config.setupOption[parseInt(choice) - 1]) {
     case "Setup Environment": {
       await setupToBashrc()
       await setupDefaultDirAndFile()
       await setupSymlink()
-      await setupApp()
       await setupService()
     } break
     case "Install Default Pkg": {
-      $.verbose = true
-      await $`sudo apt install -y curl wget ffmpeg`
-      $.verbose = false
+      echo("\nInstall Default Pkg")
+
+      await $`sudo apt install -y ${config.defaultPkg}`
+      echo(` -> Ok: installed -> ${config.defaultPkg.join(", ")}`)
     } break
-    case "Install Pkg": await installPackage(); break
+    case "Install Apps": await listApp(); break
     default: echo("Incorrect option")
   }
-}
-
-async function installPackage() {
-  echo("\n Instal Package")
-  const path = "script/install-script"
-  const pkgList = [
-    "Curl:curl:--version:apt:curl",
-    "Wget:wget:--version:apt:wget",
-    "Git:git:--version:apt:git",
-
-    "VS Code:code:-v:snap:code --classic",
-    "Sublime:subl:-v:snap:sublime-text --classic",
-    "Docker:docker:-v:file:docker.sh",
-    "Go( gvm ):gvm:version:file:go-gvm.sh",
-    "Kubectl:kubectl:version --client:file:kubectl.sh",
-    "Minikube:minikube:version:file:minikube.sh",
-    "AWS cli:aws:--version:file:aws-cli.sh",
-
-    "Obsidian:command:-v obsidian:snap:obsidian --classic",
-    "Google Chrome:google-chrome:--version:file:google-chrome.sh",
-    "Postman:command:-v postman:snap:postman",
-    "MongoDB Compass:mongodb-compass:--version:file:mongodb-compass.sh",
-    "Another Redis Desktop Manager:command:-v another-redis-desktop-manager:snap:another-redis-desktop-manager",
-    "Signal Desktop:command:-v signal-desktop:file:signal.sh",
-    "Slack:slack:-v:snap:slack",
-  ]
-  const installedPkg = []
-  const availablePkg = []
-  for (const pkg of pkgList) {
-    const [label, name, arg, type, installCommand] = pkg.split(":")
-    if (await isInstalled(name, arg)) installedPkg.push(pkg)
-    else availablePkg.push(pkg)
-  }
-
-  echo("\n\nInstalled Pkg")
-  installedPkg.forEach((pkg, i) => echo(`${i + 1}. ${pkg.split(":")[0]}`))
-
-  echo("\nAvailable Pkg")
-  availablePkg.forEach((pkg, i) => echo(`${i + 1}. ${pkg.split(":")[0]}`))
-
-  const choice = await question('Choose (space separated): ')
-  $.verbose = true
-  await $`sudo apt update`
-  for (const pkg of choice.split(" ")) {
-    const [label, name, arg, type, installCommand] = availablePkg[parseInt(pkg) - 1].split(":")
-    echo(`Install ${label}`)
-    switch (type) {
-      case "apt": await $`sudo apt install ${["-y", installCommand]}`; break;
-      case "snap": await $`sudo snap install ${installCommand}`; break;
-      case "file": await $`bash script/multiline-install-script/${installCommand}`; break;
-    }
-  }
-  $.verbose = false
-}
-
-async function isInstalled(cmd, arg) {
-  const result = await $`${cmd} ${arg.split(" ")}`.exitCode
-  return result == 0
 }
 
 async function setupToBashrc() {
@@ -94,10 +37,6 @@ async function setupToBashrc() {
   if (homeBashrc.includes(comment)) return echo(" -> Ok")
   fs.writeFileSync(bashrcPath, homeBashrc + `\n${comment}\n${command}\n`)
   echo(" -> Ok")
-}
-
-async function setupDefaultPkg() {
-
 }
 
 async function setupDefaultDirAndFile() {
@@ -137,35 +76,8 @@ async function setupSymlink() {
   }
 }
 
-async function setupApp() {
-  echo("\nSetup app")
-  for (const app of config.appList) {
-    if (!fs.existsSync(app.bin)) {
-      echo(` -> Ok: not exists -> ${app.name} binary`)
-      continue
-    }
-    if (app.systemctlScript) {
-      if (!fs.existsSync(`/etc/systemd/system/${app.name}.service`)) {
-        const serviceFile = fs.readFileSync(app.systemctlScript).toString().replaceAll("__USER__", os.userInfo().username)
-        fs.writeFileSync(`tmp/${app.name}.service`, serviceFile )
-        await $`sudo mv tmp/${app.name}.service /etc/systemd/system/${app.name}.service`
-        echo(` -> Ok: created -> /etc/systemd/system/${app.name}.service`)
-        await $`sudo systemctl daemon-reload`
-      } else echo(` -> Ok: exists -> /etc/systemd/system/${app.name}.service`)
-      if ( await $`sudo systemctl is-enabled ${app.name}`.exitCode != 0) {
-        await $`sudo systemctl enable ${app.name}`
-        echo(` -> Ok: systemctl enable -> ${app.name}`)
-      } else echo(` -> Ok: systemctl is-enabled -> ${app.name}`)
-      if ( await $`sudo systemctl is-active ${app.name}`.exitCode != 0) {
-        await $`sudo systemctl start ${app.name}`
-        echo(` -> Ok: systemctl start -> ${app.name}`)
-      } else echo(` -> Ok: systemctl is-active -> ${app.name}`)
-    }
-  }
-}
 async function setupService() {
   echo("\nSetup service")
-
   for (const dir of ["app", "manager", "storage"])
     fs.cpSync(`service/${dir}/.env.example`, `service/${dir}/.env`, { force: false })
   echo(" -> Ok: created .env copies")
@@ -179,4 +91,77 @@ async function setupService() {
   for (const vol of ["dev-app", "dev-manager", "dev-storage"])
     await $`sudo docker volume create ${vol}`.nothrow()
   echo(" -> Ok: create volume")
+}
+
+async function listApp() {
+  const yetToInstall = []
+  let counter = 0
+  echo("\nAlready installed apps")
+  for (const app of config.appList) {
+    if (await isAppInstalled(app)) echo(`${++counter}. ${app.name}`)
+    else yetToInstall.push(app)
+  }
+
+  if (yetToInstall.length == 0) return
+  counter = 0
+
+  echo("\nYet to install apps")
+  echo(`0. All`)
+  for (const app of yetToInstall) {
+    echo(`${++counter}. ${app.name}`)
+  }
+  const choiceList = (await question('Choose: ')).split(" ").map(x => parseInt(x) - 1)
+  if (choiceList.length == 0) return
+
+  counter = 0
+  const toInstall = choiceList.includes(-1) ? yetToInstall : yetToInstall.filter((_, index) => choiceList.includes(index))
+  for (const app of toInstall) {
+    echo("")
+    echo(`- Ok(${++counter}/${toInstall.length}): installing -> ${app.name}`)
+    await installApp(app)
+    if ( app.systemctl ) await setupSystemCtl(app)
+    echo(`- Ok(${counter}/${toInstall.length}): installed -> ${app.name}`)
+  }
+}
+
+async function isAppInstalled(app) {
+  return await $`ls ${app.bin}`.exitCode == 0
+}
+
+async function installApp(app) {
+  switch (app.install.from) {
+    case "snap": {
+      await $`sudo snap install ${app.install.pkg}`
+      break
+    }
+    case "file": {
+      await $`sh ${app.install.path}`
+      break
+    }
+  }
+}
+
+async function setupSystemCtl(app) {
+  echo(`  - systemctl`)
+
+  // Create systemd service if not exist
+  if (await $`ls /etc/systemd/system/${systemctl.name}.service`.exitCode != 0) {
+    const serviceFile = fs.readFileSync(systemctl.script).toString().replaceAll("__USER__", os.userInfo().username)
+    fs.writeFileSync(`${systemctl.name}.service`, serviceFile)
+    await $`sudo mv ${systemctl.name}.service /etc/systemd/system/${systemctl.name}.service`
+    echo(`    Ok: created -> ${systemctl.name}.service`)
+    await $`sudo systemctl daemon-reload`
+  } else echo(`    Ok: exists -> ${systemctl.name}.service`)
+
+  // Try to start service
+  if (await $`sudo systemctl is-enabled ${systemctl.name}`.exitCode != 0) {
+    await $`sudo systemctl enable ${systemctl.name}`
+    echo(`    Ok: enabled`)
+  } else echo(`    Ok: is-enabled`)
+
+  // Try to start service
+  if (await $`sudo systemctl is-active ${systemctl.name}`.exitCode != 0) {
+    await $`sudo systemctl start ${systemctl.name}`
+    echo(`    Ok: started`)
+  } else echo(`    Ok: is-active`)
 }
